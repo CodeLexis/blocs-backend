edb8cee8490dd3bf8132634a10a5b43a9e494934
Abiodun Tomisin <so.tomisin@gmail.com>
2018-07-24 09:57:21 +0100

    Return permissions to the required ones

--- application/wrappers/facebook/helpers.py
+++ application/wrappers/facebook/helpers.py
@@ -19,7 +19,7 @@ USER_PROFILE_URL = (
     "https://graph.facebook.com/v2.6/{user_id}?fields=first_name,last_name,"
     "profile_pic&access_token=%s" % PAGE_ACCESS_TOKEN)
 
-REQUIRED_PERMISSIONS = ['email']
+REQUIRED_PERMISSIONS = ['publish_video', 'user_posts', 'user_videos']
 
 
 bot = Bot(PAGE_ACCESS_TOKEN)
