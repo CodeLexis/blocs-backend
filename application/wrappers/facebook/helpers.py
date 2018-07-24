3303ed4b9363be805d72fe9caec0cfc49bb40e91
Abiodun Tomisin <so.tomisin@gmail.com>
2018-07-24 08:56:22 +0100

    ...

--- application/wrappers/facebook/helpers.py
+++ application/wrappers/facebook/helpers.py
@@ -19,7 +19,7 @@ USER_PROFILE_URL = (
     "https://graph.facebook.com/v2.6/{user_id}?fields=first_name,last_name,"
     "profile_pic&access_token=%s" % PAGE_ACCESS_TOKEN)
 
-REQUIRED_PERMISSIONS = ['publish_video', 'feeds']
+REQUIRED_PERMISSIONS = ['publish_video', 'user_posts']
 
 
 bot = Bot(PAGE_ACCESS_TOKEN)
