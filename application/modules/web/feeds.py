aed7cadf4d1a6b752452e0a45fcc24cc8fa4e776
Abiodun Tomisin <so.tomisin@gmail.com>
2018-07-26 14:32:11 +0100

    ...

--- application/modules/web/feeds.py
+++ application/modules/web/feeds.py
@@ -1,14 +1,5 @@
-from flask import g, redirect, url_for
-from dateutil import parser as date_parser
-from flask import Response
-import requests
-
 from application.core.models import FeedLike
-from application.core.models import prep_paginate_query, get_pagination_meta
-from application.core.utils.request_response_helpers import (
-    get_request_pagination_params)
-from application.events import create_event, declare_event_interest
-from . import render_template, request, web_blueprint
+from . import render_template, web_blueprint
 
 
 @web_blueprint.route('/feeds/<int:feed_id>/likes', methods=['GET', 'POST'])
@@ -20,4 +11,5 @@ def render_all_feeds_likes(feed_id):
     for feed_like in feed_likes:
         users.append(feed_like.user.as_json())
 
-    return render_template('users_list.html', users=users)
+    return render_template('users_list.html', title='People who also liked it',
+                           users=users)
